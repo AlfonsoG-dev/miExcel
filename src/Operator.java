0b5c395f2ddc320d7559e18fcf6ad793ee6e9a89
tuNovio <alfgomachi@gmail.com>
2023-11-17 01:17:26 -0500

    feat: aggregate the aritmetic operations [*/+-]

---
+++ src/Operator.java
@@ -0,0 +1,105 @@
+public record Operator() {
+    
+    /**
+     * columns declare on top of the file 
+     */
+    public static String getColumns(String fileText) {
+        String[] fileLines = fileText.split("\n");
+        String columns ="";
+        String[] co = fileLines[0].split("\\.");
+        for(String c: co) {
+            columns += c + "\n";
+        }
+        return columns;
+    }
+    /**
+     * last column have the operations between prev columns
+     */
+    public static String getRowsOfColumn(String fileText, String column) {
+        String[] fileLines = fileText.split("\n");
+        String rows = "";
+        String[] columns = fileLines[0].split("\\.");
+        for(int i=0; i<columns.length; ++i) {
+            if(columns[i].equals(column)) {
+                for(int j=1; j<fileLines.length; ++j) {
+                    rows += fileLines[j].split("\\.")[i]  + "\n";
+                }
+            }
+        }
+        return rows;
+    }
+    public static int Sumar(String fileText, String sentences) {
+        String[] spaces = sentences.split("\\+");
+        int res = 0, sum = 0;
+        for(String s: spaces) {
+            String[] data = s.split("");
+            String co = data[0];
+            int p = Integer.parseInt(data[1])-1;
+            String[] rows = getRowsOfColumn(fileText, "<" + co + ">").split("\n");
+            sum += Integer.parseInt(rows[p].replaceAll("[<>]", ""));
+            res = sum;
+        }
+        System.out.println(sentences + "=" + res);
+        return res;
+    }
+    public static int Multiplicar(String fileText, String sentences) {
+        String[] spaces = sentences.split("\\*");
+        int res = 0, mult = 1;
+        for(String s: spaces) {
+            String[] data = s.split("");
+            String co = data[0];
+            int p = Integer.parseInt(data[1])-1;
+            String[] rows = getRowsOfColumn(fileText, "<" + co + ">").split("\n");
+            mult *= Integer.parseInt(rows[p].replaceAll("[<>]", ""));
+            res = mult;
+        }
+        System.out.println(sentences + "=" + res);
+        return res;
+    }
+    public static float Dividir(String fileText, String sentences) {
+        String[] spaces = sentences.split("\\/");
+        int div = 1;
+        float res = 0;
+        String nums = "";
+        for(String s: spaces) {
+            String[] data = s.split("");
+            String co = data[0];
+            int p = Integer.parseInt(data[1])-1;
+            String[] rows = getRowsOfColumn(fileText, "<" + co + ">").split("\n");
+            div = Integer.parseInt(rows[p].replaceAll("[<>]", ""));
+            nums += div + "\n";
+        }
+        String[] mNums = nums.split("\n");
+        int inicial = 0, siguiente = 0;
+        for(int i=1; i<mNums.length; ++i) {
+            inicial = Integer.parseInt(mNums[0]);
+            siguiente += inicial / Integer.parseInt(mNums[i]);
+            res = siguiente/mNums.length-1;
+        }
+        System.out.println(sentences + "=" + res);
+        return res;
+    }
+    public static int Restar(String fileText, String sentences) {
+        String[] spaces = sentences.split("\\-");
+        int div = 1;
+        int res = 0;
+        String nums = "";
+        for(String s: spaces) {
+            String[] data = s.split("");
+            String co = data[0];
+            int p = Integer.parseInt(data[1])-1;
+            String[] rows = getRowsOfColumn(fileText, "<" + co + ">").split("\n");
+            div = Integer.parseInt(rows[p].replaceAll("[<>]", ""));
+            nums += div + "\n";
+        }
+        String[] mNums = nums.split("\n");
+        int inicial = 0, siguiente = 0;
+        for(int i=1; i<mNums.length; ++i) {
+            inicial = Integer.parseInt(mNums[0]);
+            siguiente += Integer.parseInt(mNums[i]);
+        }
+        res = inicial - siguiente;
+        System.out.println(sentences + "=" + res);
+        return res;
+    }
+}
--- src/miExcel.java
+++ src/miExcel.java
@@ -3,10 +3,10 @@ import java.io.File;
 import java.io.FileReader;
 
 class miExcel {
+    static String path = "./docs/miExcelFile.txt";
     public static void main(String[] args) {
         try {
-            String path = "./docs/miExcelFile.txt";
-            getOperations(path);
+            getOperations();
         } catch(Exception e) {
             System.err.println(e);
         }
@@ -39,79 +39,31 @@ class miExcel {
         return fileLines;
     }
     /**
-     * columns declare on top of the file 
-     */
-    static String getColumns(String filePath) {
-        String[] fileLines = readFileText(filePath).split("\n");
-        String columns ="";
-        String[] co = fileLines[0].split("\\.");
-        for(String c: co) {
-            columns += c + "\n";
-        }
-        return columns;
-    }
-    /**
-     * last column have the operations between prev columns
-     */
-    static String getRowsOfColumn(String filePath, String column) {
-        String[] fileLines = readFileText(filePath).split("\n");
-        String rows = "";
-        String[] columns = fileLines[0].split("\\.");
-        for(int i=0; i<columns.length; ++i) {
-            if(columns[i].equals(column)) {
-                for(int j=1; j<fileLines.length; ++j) {
-                    rows += fileLines[j].split("\\.")[i]  + "\n";
-                }
-            }
-        }
-        return rows;
-    }
-    static void Sumar(String filePath, String operations, String mainOp) throws Exception {
-        String[] spaces = operations.split("");
-        String[] operador = new String[2];
-        for(int i=0; i<spaces.length; ++i) {
-        String mColumns = "";
-        int position = 0;
-            if(mainOp == "sumar") {
-             mColumns = spaces[i].matches("[+^1-9]") == false ? spaces[i] : "";
-             position = spaces[i].matches("[+^A-Z]") == false ? Integer.parseInt(spaces[i]): -1;
-            } else {
-                throw new Exception("not implemented yet");
-            }
-            if(mColumns != "") {
-                operador[0] += mColumns + "\n";
-            }
-            if(position != -1) {
-                operador[1] += position + "\n";
-            }
-        }
-        String[] columns = operador[0].replace("null", "").split("\n");
-        String[] position = operador[1].replace("null", "").split("\n");
-        int res = 0;
-        for(int i=0; i<columns.length; ++i) {
-            String co = columns[i];
-            int p = Integer.parseInt(position[i])-1;
-            String[] rows = getRowsOfColumn(filePath, "<" + co + ">").split("\n");
-            res += Integer.parseInt(rows[p].replaceAll("[<>]", ""));
-        }
-        System.out.println(operations.concat("=" + res));
-    }
-    /**
      * @throws Exception
      */
-    static String getOperations(String filePath) throws Exception {
+    static String getOperations() throws Exception {
         String operation = "";
-        String[] fileLines = readFileText(filePath).split("\n");
+        String fileText = readFileText(path);
+        String[] fileLines = readFileText(path).split("\n");
         for(int i=1; i<fileLines.length; ++i) {
             String[] rows = fileLines[i].split("\\.");
             String row = rows[rows.length-1].replaceAll("[<.>=]", "");
             String[] op = row.split("");
             for(int o=0; o<op.length; ++o) {
-                if(op[o].matches("[+]") && op.length < 8) {
-                    Sumar(filePath, row, "sumar");
-                    //operation += op[o-2].concat(op[o-1]) + op[o] + op[o+1].concat(op[o+2]) + "\n";
-                } else if(op.length >= 8) {
-                    Sumar(filePath, row, "sumar");
+                if(op[o].matches("[*]")) {
+                    Operator.Multiplicar(fileText, row);
+                    break;
+                }
+                if(op[o].matches("[\\/]")) {
+                    Operator.Dividir(fileText, row);
+                    break;
+                }
+                if(op[o].matches("[+]")) {
+                    Operator.Sumar(fileText, row);
+                    break;
+                }
+                if(op[o].matches("[\\-]")) {
+                    Operator.Restar(fileText, row);
                     break;
                 }
             }
